4e98c477375e31479c0ac3bfd1ff0e16bc9e28a7
Roberto Mejía <robertomejiacollazos@gmail.com>
2024-11-21 17:49:00 -0500

    4/50

---
+++ clase_02/src/main/java/clase_02/ejercicios/Ejercicios.java
@@ -0,0 +1,87 @@
+package clase_02.ejercicios;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		// EJERCICIO 1
+		int num1 = -2;
+		if(num1 >= 0) {
+			System.out.println("EJERCICIO 1: " + num1 + " es positivo.");
+		} else {
+			System.out.println("EJERCICIO 1: " + num1 + " es negativo.");
+		};
+		
+		// EJERCICIO 2
+		int edadPersona = 18;
+		if(edadPersona >= 18) {
+			System.out.println("EJERCICIO 2: " + edadPersona + " es mayor de edad.");			
+		}else {
+			System.out.println("EJERCICIO 2: " + edadPersona + " es menor de edad.");						
+		};
+		// EJERCICIO 3
+		int numImpar = 15;
+		if(numImpar % 2 == 0) {			
+			System.out.println("EJERCICIO 3: " + numImpar + " es par.");						
+		}else {
+			System.out.println("EJERCICIO 3: " + numImpar + " es inpar.");						
+		};
+		// EJERCICIO 4
+		String cadena = "";
+		if (cadena.isEmpty()) {
+			System.out.println("EJERCICIO 4: esta vacia.");									
+		}else {
+			System.out.println("EJERCICIO 4: esta con caracteres.");									
+			
+		}
+		// EJERCICIO 5
+
+		// EJERCICIO 6
+		// EJERCICIO 7
+		// EJERCICIO 8
+		// EJERCICIO 9
+		// EJERCICIO 10
+		// EJERCICIO 11
+		// EJERCICIO 12
+		// EJERCICIO 13
+		// EJERCICIO 14
+		// EJERCICIO 15
+		// EJERCICIO 16
+		// EJERCICIO 17
+		// EJERCICIO 18
+		// EJERCICIO 19
+		// EJERCICIO 20
+		// EJERCICIO 21
+		// EJERCICIO 22
+		// EJERCICIO 23
+		// EJERCICIO 24
+		// EJERCICIO 25
+		// EJERCICIO 26
+		// EJERCICIO 27
+		// EJERCICIO 28
+		// EJERCICIO 29
+		// EJERCICIO 30
+		// EJERCICIO 31
+		// EJERCICIO 32
+		// EJERCICIO 33
+		// EJERCICIO 34
+		// EJERCICIO 35
+		// EJERCICIO 36
+		// EJERCICIO 37
+		// EJERCICIO 38
+		// EJERCICIO 39
+		// EJERCICIO 40
+		// EJERCICIO 41
+		// EJERCICIO 42
+		// EJERCICIO 43
+		// EJERCICIO 44
+		// EJERCICIO 45
+		// EJERCICIO 46
+		// EJERCICIO 47
+		// EJERCICIO 48
+		// EJERCICIO 49
+		// EJERCICIO 50
+		
+		
+	}
+
+}
