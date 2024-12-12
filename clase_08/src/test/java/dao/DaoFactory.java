0c715ebe9dc535914c05059a57d5c86150c11fc7
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2024-12-12 15:12:55 -0500

    clase 08

---
+++ clase_08/src/test/java/dao/DaoFactory.java
@@ -0,0 +1,28 @@
+package dao;
+
+
+import org.springframework.stereotype.Service;
+
+import models.Alumno;
+import models.Curso;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Service
+public class DaoFactory {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Transactional
+	public void persistirAlumno(Alumno alumno) {
+		em.persist(alumno);
+	}
+
+	@Transactional
+	public void persistirCurso(Curso curso) {
+		em.persist(curso);
+	}
+}
